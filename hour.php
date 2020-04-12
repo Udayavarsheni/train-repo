f6e0836b82a7fb596ace5ac052a0348ac2c2f747
Udayavarsheni <udayavarsheni@gmail.com>
2020-04-12 11:19:42 +0530

    first commit

---
+++ hour.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<?php
+$t = 15;
+
+if ($t < "10") {
+    echo "Have a good morning!";
+} elseif ($t < "20") {
+    echo "Have a good day!";
+} else {
+    echo "Have a good night!";
+}
+?>
+ 
+</body>
+</html>
